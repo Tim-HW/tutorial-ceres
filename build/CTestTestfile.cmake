e73ddb224bdf0468440046439f133e5c9ac2dc81
Tim-Desktop <timothee.freville@mil.be*>
2024-01-16 13:06:13 +0100

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tim/DREAM/test
+# Build directory: /home/tim/DREAM/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
